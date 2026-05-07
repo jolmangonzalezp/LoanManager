18fddcf0b0c9df3dca3f83655ee99029bf683e89
jolmangonzalezp <jolmangonzalez5@gmail.com>
2026-05-06 21:43:38 -0500

    Entrypoint modified

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Generar caché de configuración para velocidad
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Correr migraciones automáticamente
 echo "Ejecutando migraciones en la base de datos..."
 php artisan migrate --force
 
-# Iniciar Apache en primer plano
 exec apache2-foreground
